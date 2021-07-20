1831cea4bff0fc190649747bca6df03abd3c98ed
omid5643 <seyed.abaei@yahoo.com>
2021-07-20 16:26:21 -0500

    added some methods to ScheduleService

--- Omran.Sama.Services/ScheduleService.cs
+++ Omran.Sama.Services/ScheduleService.cs
@@ -1,6 +1,9 @@
-﻿using Omran.Sama.Commen.Constants;
+﻿using Newtonsoft.Json;
+using Omran.Sama.Commen.Constants;
+using Omran.Sama.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,72 @@ namespace Omran.Sama.Services
     {
         // this is the database path
         private readonly string fullPath = DbConstants.DbPath + DbConstants.ScheduleFile;
+        private bool Store(List<Schedule> schedules)
+        {
+            try
+            {
 
+                string serialized = JsonConvert.SerializeObject(schedules);
+                File.WriteAllText(this.fullPath, serialized);
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                Loger.Log(e.Message);
+                return false;
+
+            }
+
+
+
+        }
+
+       public List<Schedule> Load()
+        {
+            if (File.Exists(this.fullPath))
+            {
+
+                string content = File.ReadAllText(this.fullPath);
+                List<Schedule> schedules = JsonConvert.DeserializeObject<List<Schedule>>(content);
+
+                return schedules ;
+            }
+            return null;
+
+        }
+       public  Schedule LoadById(int id)
+        {
+            try
+            {
+                List<Schedule> schedules = Load();
+                var matched = schedules.Single(x => x.Id == id);
+
+                return matched;
+            }
+
+            catch (Exception e)
+            {
+                Loger.Log(e.Message);
+                return null;
+            }
+
+        }
+      public  bool Add(Schedule schedule)
+        {
+
+        }
+        bool Remove(int id)
+        {
+
+
+        }
+        bool Update(Schedule schedule)
+        {
+
+
+
+        }
 
 
     }
