569c94a006f6ccf9e950cf7027674399bf582c5f
omid5643 <seyed.abaei@yahoo.com>
2021-07-20 13:25:38 -0500

    Added Comment

--- Omran.Sama.Server/Startup.cs
+++ Omran.Sama.Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc.Core;
 
 namespace Omran.Sama.Server
 {
+    //add
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
