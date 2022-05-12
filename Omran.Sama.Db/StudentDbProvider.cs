4fddb77a5140b0de89b7670b1585e06398142174
omid5643 <seyed.abaei@yahoo.com>
2022-05-12 13:41:16 -0500

    feat: add data layer

---
+++ Omran.Sama.Db/StudentDbProvider.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Omran.Sama.Commen.Constants;
+using Omran.Sama.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Omran.Sama.Db
+{
+    public class StudentDbProvider
+    {
+        private readonly string fullPath = DbConstants.DbPath + DbConstants.StudentFile;
+        public bool Store(List<Student> students)
+        {
+            try
+            {
+
+                string serialized = JsonConvert.SerializeObject(students);
+                File.WriteAllText(this.fullPath, serialized);
+                return true;
+            }
+
+            catch (Exception e)
+            {
+               // Loger.Log(e.Message);
+                return false;
+
+            }
+        }
+
+        public List<Student> Load()
+        {
+            if (File.Exists(this.fullPath))
+            {
+
+                string content = File.ReadAllText(this.fullPath);
+                List<Student> students = JsonConvert.DeserializeObject<List<Student>>(content);
+
+                return students;
+            }
+            return null;
+        }
+
+
+    }
+}
