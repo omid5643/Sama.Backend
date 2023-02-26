db94eb5c8e3c981639cd1ef589a9da5e0acbd9f1
omid5643 <seyed.abaei@yahoo.com>
2023-02-26 12:19:05 -0600

    fix: test project compile error

--- Omran.Sama.Models/Instructor.cs
+++ Omran.Sama.Models/Instructor.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace Omran.Sama.Models
 {
-   public class Instructor:Entity
+    public class Instructor : Entity
     {
-      
-        public string FirstName {get; set;}
-        public string LastName {get; set;}
-        public Address Address {get; set;}
-        public string[] PhoneNumber {get; set;}
-        public string Email { get; set; }
-        public int UserId {get; set;}
-        public DateTime CreateDate {get; set;}
-        public string CreateBy {get; set;}
 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+        public string[] PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreateBy { get; set; }
+       
 
     }
 }
